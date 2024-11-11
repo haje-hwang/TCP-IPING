e8e268482f67543d22de45a13c9866c572873e54
gny0922 <gny0922@gmail.com>
2024-11-11 15:15:12 +0900

    방 입장하기 판넬

--- TCP-IPING/Assets/Scripts/Lobby/LoginManager.cs
+++ TCP-IPING/Assets/Scripts/Lobby/LoginManager.cs
@@ -1,12 +1,15 @@
+using TMPro;
 using UnityEditor.XR;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class LoginManager : MonoBehaviour
 {
-    public InputField NicknameInput; // 닉네임 입력 필드
+    public TMP_InputField nicknameInputField; // 닉네임 입력 필드
     public Button RoomCreate;  // 방 만들기 버튼
     public Button RoomJoin;    // 방 입장 버튼
+    public TMP_InputField RoomJoinInput;
+    public GameObject panel;
 
     private string playerNickname;
 
@@ -14,12 +17,12 @@ public class LoginManager : MonoBehaviour
     {
         // 버튼 클릭 시 호출할 함수 연결
         RoomCreate.onClick.AddListener(OnCreateRoom);
-        //RoomJoin.onClick.AddListener(OnJoinRoom);
+        RoomJoin.onClick.AddListener(OnJoinRoom);
     }
 
     public void SaveNickname()
     {
-        playerNickname = NicknameInput.text;
+        playerNickname = nicknameInputField.text;
         PlayerPrefs.SetString("PlayerNickname", playerNickname); // 닉네임을 저장
         Debug.Log("닉네임 저장됨: " + playerNickname);
     }
@@ -29,7 +32,13 @@ public class LoginManager : MonoBehaviour
         SaveNickname();
     }
 
-
+    void OnJoinRoom()
+    {
+ 
+        panel.SetActive(true);  // Panel을 다시 보이게 함
+        
+    }
 }
+
 
 
