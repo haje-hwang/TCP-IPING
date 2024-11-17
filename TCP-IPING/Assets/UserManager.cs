786e70926052105b02a280a17ee89e4fd8c1e1fb
01_02hyun <0714doo52@naver.com>
2024-11-17 20:15:48 +0900

    샘플씬 기본 구현 완료

--- TCP-IPING/Assets/UserManager.cs
+++ TCP-IPING/Assets/UserManager.cs
@@ -4,10 +4,10 @@ using TMPro; // TextMeshPro 네임스페이스 추가
 public class UserManager : MonoBehaviour
 {
     public UserList userList; // UserList Singleton 연결
-    /*
+    
     public TMP_InputField nameInputField; // 유저 이름을 입력받는 TMP_InputField
     public TMP_Text feedbackText; // 결과 메시지를 표시할 TMP_Text 컴포넌트
-    */
+    
     // 버튼 클릭 시 호출될 메서드
     public void OnCreateUserButtonClick()
     {
@@ -16,19 +16,32 @@ public class UserManager : MonoBehaviour
             UnityEngine.Debug.LogError("UserList가 초기화되지 않았습니다!");
             return;
         }
-        /*
+        
         string userName = nameInputField.text; // 입력 필드에서 유저 이름 가져오기
         if (string.IsNullOrWhiteSpace(userName))
         {
             feedbackText.text = "이름을 입력하세요!";
             return;
         }
-        */
+        
         // UserList의 CreateNewUser 호출
-        User newUser = userList.CreateNewUser();
+        User newUser = userList.CreateNewUser(userName);
 
         // 결과 메시지 표시
-        //feedbackText.text = $"새 유저 생성 완료: {newUser.name} (ID: {newUser.id})";
+        feedbackText.text = $"새 유저 생성 완료: {newUser.nickName} (ID: {newUser.id})";
         UnityEngine.Debug.Log($"새 유저 생성 완료: {newUser.nickName} (ID: {newUser.id})");
+    }
+    public void OnDeleteButtonClick()
+    {
+        if (userList == null)
+        {
+            UnityEngine.Debug.LogError("UserList가 초기화되지 않았습니다!");
+            return;
+        }
+        userList.DeleteAllUsers();
+
+        // 결과 메시지 표시
+        feedbackText.text = $"모든 유저정보가 제거 되었습니다.";
+        UnityEngine.Debug.Log($"모든 유저정보가 제거 되었습니다.");
     }
 }
