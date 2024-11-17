485358ff031f87928a65e399c3c19d74a62cb4d0
gny0922 <gny0922@gmail.com>
2024-11-18 04:18:20 +0900

    씬 이동 완료

--- TCP-IPING/Assets/Scripts/Lobby/SceneLoader.cs
+++ TCP-IPING/Assets/Scripts/Lobby/SceneLoader.cs
@@ -2,11 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneLoader : MonoBehaviour
 {
+    public Button again;
+    public Button Rankinghome;
+
+    private void Start()
+    {
+
+        if (again != null)
+        {
+            again.onClick.AddListener(() => SceneManager.LoadScene("WaitingScene"));
+        }
+
+        if (Rankinghome != null)
+        {
+            Rankinghome.onClick.AddListener(() => SceneManager.LoadScene("LoginScene"));
+        }
+    }
     public void LoadLoginScene()
     {
         SceneManager.LoadScene("LoginScene"); // LoginScene으로 이동
+    }
+    public void LoadWaitingRoomScene()
+    {
+        SceneManager.LoadScene("WaitingScene"); // WaitingScene으로 이동
+    }
+    public void PlayScene()
+    {
+        SceneManager.LoadScene("PlayScene"); // PlayScene으로 이동
     }
 }
---
+++ TCP-IPING/Assets/Scripts/Lobby/WaitScene.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class WaitingScene : MonoBehaviour
+{
+    [SerializeField] private Button WaitingPlay;  // WaitingScene에서 사용할 Play 버튼
+    [SerializeField] private Button WaitingHome;  // WaitingScene에서 사용할 Home 버튼
+
+    private void Start()
+    {
+        if (WaitingPlay != null)
+        {
+            WaitingPlay.onClick.AddListener(() => SceneManager.LoadScene("PlayScene"));
+        }
+
+        if (WaitingHome != null)
+        {
+            WaitingHome.onClick.AddListener(() => SceneManager.LoadScene("LoginScene"));
+        }
+    }
+}
