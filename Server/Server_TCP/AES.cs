8b8de33af81f24744b510f8ce23ae53367abcb34
haje hwang <haje0001@gmail.com>
2024-12-02 16:59:43 +0900

    Create AES.cs
    만약 통신에 암호화 추가하고 싶으면 쓰기

---
+++ Server/Server_TCP/AES.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Server_TCP
+{
+    class AES
+    {
+        public const string IV = "InitVector123456";   //should 16byte
+        public static string GenerateRandomKey()
+        {
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                byte[] keyBytes = new byte[32]; // AES-256용 32바이트
+                rng.GetBytes(keyBytes);
+                string key = Convert.ToBase64String(keyBytes);
+                return key;
+            }
+        }
+        public static string Encrypt(string plainText, string key, string iv)
+        {
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = Encoding.UTF8.GetBytes(key);
+                aes.IV = Encoding.UTF8.GetBytes(iv);
+
+                using (MemoryStream memoryStream = new MemoryStream())
+                {
+                    using (CryptoStream cryptoStream = new CryptoStream(memoryStream, aes.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        byte[] plainBytes = Encoding.UTF8.GetBytes(plainText);
+                        cryptoStream.Write(plainBytes, 0, plainBytes.Length);
+                        cryptoStream.FlushFinalBlock();
+
+                        return Convert.ToBase64String(memoryStream.ToArray());
+                    }
+                }
+            }
+        }
+
+        public static string Decrypt(string cipherText, string key, string iv)
+        {
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = Encoding.UTF8.GetBytes(key);
+                aes.IV = Encoding.UTF8.GetBytes(iv);
+
+                using (MemoryStream memoryStream = new MemoryStream())
+                {
+                    using (CryptoStream cryptoStream = new CryptoStream(memoryStream, aes.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        byte[] cipherBytes = Convert.FromBase64String(cipherText);
+                        cryptoStream.Write(cipherBytes, 0, cipherBytes.Length);
+                        cryptoStream.FlushFinalBlock();
+
+                        return Encoding.UTF8.GetString(memoryStream.ToArray());
+                    }
+                }
+            }
+        }
+    }
+}
